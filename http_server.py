bdff1b9ab6525430e7d802c554aafd0da9f9e154
Alec Kriebel <alec@instamour.com>
2016-10-22 00:47:09 -0700

    Added progress so far

---
+++ http_server.py
@@ -0,0 +1,53 @@
+# Your EECS account information is:
+# Login:	akriebel
+# Password:	normal pword
+
+#import socket module
+from socket import *
+import threading
+
+def handler(connectionSocket, addr):
+	try:
+		message = connectionSocket.recv(1024).decode()
+		filename = None
+		if len(message.split()) > 1:
+			filename = message.split()[1]
+		else:
+			filename = 'index.html'
+		f = open(filename[1:], "rb") #TODO? Will fail if no file specified (should get index.html)
+
+		outputdata = f.read()
+
+		connectionSocket.send('HTTP/1.1 200 OK\n'.encode('utf-8'))
+		connectionSocket.send('Content-Type: text/html\n'.encode('utf-8'))
+		connectionSocket.send('\n'.encode('utf-8'))
+
+		#Send the content of the requested file to the client
+		for i in range(0,len(outputdata)):
+			connectionSocket.send(outputdata[i:i+1])
+		connectionSocket.send(b'\r\n\r\n')
+
+		f.close()
+		connectionSocket.close()
+	except IOError:
+		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode('utf-8'))
+		connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode('utf-8'))
+
+		f.close()
+		connectionSocket.close()
+
+def main():
+	serverSocket = socket(AF_INET, SOCK_STREAM)
+	# Prepare a sever socket
+	serverSocket.bind(('', 6789))
+	serverSocket.listen(1)
+	while True:
+		print ("Ready to serve...")
+		connectionSocket, addr = serverSocket.accept()
+		thread = threading.Thread(target=handler, args=(connectionSocket, addr))
+		thread.start()
+
+	serverSocket.close()
+
+if __name__ == '__main__':
+	main()
---
+++ smtp_client.py
@@ -0,0 +1,59 @@
+from socket import *
+
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = ('localhost', 25)
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(mailserver)
+recv = clientSocket.recv(1024).decode()
+print(recv)
+
+if recv[:3] != '220':
+	print('220 reply not received from server.')
+
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+	print('250 reply not received from server.')
+ 
+# Send MAIL FROM command and print server response.
+clientSocket.send('MAIL FROM: <akriebel@uci.edu>\r\n'.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250': #if the data is not received
+	print('250 reply not received from server.')
+
+# Send RCPT TO command and print server response.
+clientSocket.send('RCPT TO: <akriebel@uci.edu>\r\n'.encode())
+recv1 = clientSocket.recv(1024)
+print(recv1)
+if recv1[:3] != '250':
+	print('250 reply not received from server.')
+
+# Send DATA command and print server response.
+clientSocket.send('DATA\r\n'.encode())
+recv1 = clientSocket.recv(1024)
+print(recv1)
+if recv1[:3] != '354':
+	print('250 reply not received from server.')
+
+# Send message data.
+clientSocket.send(msg)
+
+# Message ends with a single period.
+clientSocket.send(endmsg)
+recv1 = clientSocket.recv(1024)
+print(recv1)
+if recv1[:3] != '250':
+	print('250 reply not received from server.')
+
+# Send QUIT command and get server response.
+clientSocket.send('QUIT\r\n'.encode())
+clientSocket.close()
